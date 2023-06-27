3e75b8bdb676efeef504b906e1318653e847b300
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-26 23:08:36 -0600

    add exc 4 code

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -12,7 +12,11 @@ Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, wome
 Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true )
 
 @men_stores = Store.where(mens_apparel: true)
-
 @men_stores.each do |store|
+  puts "Store #{store.name} earns #{store.annual_revenue} of annual revenue"
+end
+
+@women_stores = Store.where(womens_apparel: true).where('annual_revenue < ?', 1000000)
+@women_stores.each do |store|
   puts "Store #{store.name} earns #{store.annual_revenue} of annual revenue"
 end
