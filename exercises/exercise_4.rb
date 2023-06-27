c2c64568c4599642e4696fa3c2276cd61e293cee
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-26 23:03:13 -0600

    add exc 4 code

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,12 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true )
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false )
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true )
+
+@men_stores = Store.where(mens_apparel: true)
+
+@men_stores.each do |store|
+  puts "Store #{store.name} earns #{store.annual_revenue} of annual revenue"
+end
